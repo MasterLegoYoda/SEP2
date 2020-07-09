2b6b2d7fd8d711f448a512ec81d923ca41d10df9
Javieruw <javier.uw@gmail.com>
2020-07-09 12:10:01 +0200

    l

--- Server/src/database/InsertUserImpl.java
+++ Server/src/database/InsertUserImpl.java
@@ -2,13 +2,28 @@ package database;
 
 import sharedClasses.User;
 
-import java.sql.SQLException;
+import java.sql.*;
 
 public class InsertUserImpl implements InsertUser
 {
+    private Connection connection;
 
     @Override
     public void insertUser(User user) throws SQLException {
+        try{
+            Class.forName("org.postgresql.Driver");
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5433", "postgres",
+                    "password");
+            Statement st = connection.createStatement();
+            String query = "SELECT*FROM \"User\" WHERE id="+id+"AND password="+password;
+            ResultSet rs = st.executeQuery(query);
 
+            while(rs.next()){
+                if (rs.getInt(0)==0){response = true;}
+                else {response = false;}
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
