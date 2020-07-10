eaa69326eee593e6ea483c6b5786dcab2bba6e51
Javieruw <javier.uw@gmail.com>
2020-07-10 14:43:03 +0200

    l

--- Client/src/view/admin/AdminController.java
+++ Client/src/view/admin/AdminController.java
@@ -1,5 +1,15 @@
 package view.admin;
 
+import core.ViewHandler;
+import core.ViewModelFactory;
+
 public class AdminController
 {
+    private ViewHandler viewHandler;
+    private AdminViewModel adminViewModel;
+
+    public void init(ViewModelFactory viewModelFactory, ViewHandler viewHandler){
+    this.viewHandler = viewHandler;
+    adminViewModel = viewModelFactory.getAdminViewModel();
+    }
 }
--- Client/src/view/admin/AdminViewModel.java
+++ Client/src/view/admin/AdminViewModel.java
@@ -1,5 +1,20 @@
 package view.admin;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 public class AdminViewModel
 {
+    IntegerProperty id;
+    StringProperty status;
+
+    public AdminViewModel(){
+    id = new SimpleIntegerProperty();
+    status = new SimpleStringProperty();
+    }
+
+    public IntegerProperty getId(){return id;}
+    public StringProperty getStatus(){return status;}
 }
