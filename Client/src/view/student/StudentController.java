2900aa1720d788d89fd70a8d07e1dff0cb897e30
Javieruw <javier.uw@gmail.com>
2020-07-28 13:45:22 +0200

    l

--- Client/src/view/student/StudentController.java
+++ Client/src/view/student/StudentController.java
@@ -1,5 +1,12 @@
 package view.student;
 
-public class StudentController
-{
+import core.ViewHandler;
+
+public class StudentController{
+    private ViewHandler viewHandler;
+    private StudentViewModel studentViewModel;
+    public void init(ViewHandler viewHandler, StudentViewModel studentViewModel){
+        this.viewHandler = viewHandler;
+        this.studentViewModel = studentViewModel;
+    }
 }
--- Client/src/view/studentEmployee/StudentEmployeeController.java
+++ Client/src/view/studentEmployee/StudentEmployeeController.java
@@ -1,5 +1,14 @@
 package view.studentEmployee;
 
+import core.ViewHandler;
+import view.student.StudentViewModel;
+
 public class StudentEmployeeController
 {
+    private ViewHandler viewHandler;
+    private StudentEmployeeViewModel studentEmployeeViewModel;
+    public void init(ViewHandler viewHandler, StudentEmployeeViewModel studentEmployeeViewModel){
+        this.viewHandler = viewHandler;
+        this.studentEmployeeViewModel = studentEmployeeViewModel;
+    }
 }
